aedaef81c4204fb02824b1e65688fff493bb9e05
raschmitt <raschmitt@users.noreply.github.com>
2026-05-10 22:32:40 +0000

    Deploying to gh-pages from @ raschmitt/zapinit@0f32f4f5535f4827d1dd9b699549de44d525c553 🚀

---
+++ previews/pr-65/js/i18n/en.js
@@ -0,0 +1,10 @@
+(function () {
+    globalThis.__i18n = globalThis.__i18n || {};
+    globalThis.__i18n.en = {
+        buttonLabel: 'Open on WhatsApp',
+        phonePlaceholder: 'Phone number',
+        errorEmpty: 'Please enter a phone number',
+        errorInvalid: 'Invalid phone number',
+        aboutBlurb: 'Tired of saving a contact just to send one message? Type a number and open WhatsApp instantly, no contacts, no clutter.',
+    };
+})();
